41a0600732ba8485042749eded5277364bfd56d9
TextError <savinraul1983@yahoo.com>
2019-08-13 16:48:45 +0100

    checkout page

--- client/src/components/checkout/Checkout.js
+++ client/src/components/checkout/Checkout.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+// Redux
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { select_cart_items, select_cart_total } from '../../redux/selectors/cart';
 
 // Scss
 import './checkout.scss';
 
-const Checkout = () => (
-  <div>Checkout</div>
+const Checkout = ({ cart_item, cart_total }) => (
+  <div className='checkout-page'>
+    <div className='checkout-header'>
+      <div className='header-block'>
+        <span>Product</span>
+      </div>
+      <div className='header-block'>
+        <span>Description</span>
+      </div>
+      <div className='header-block'>
+        <span>Quantity</span>
+      </div>
+      <div className='header-block'>
+        <span>Price</span>
+      </div>
+      <div className='header-block'>
+        <span>Remove</span>
+      </div>
+    </div>
+    {
+      cart_item.map(i => i.name)
+    }
+    <div className='total'>TOTAL: ${cart_total}</div>
+    <div className='test-warning'>
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+    </div>
+  </div>
 );
 
-export default Checkout;
+Checkout.propTypes  = {
+  cart_item: PropTypes.array.isRequired,
+  cart_total: PropTypes.number.isRequired
+}
+
+const mapStateToProps = createStructuredSelector({
+  cart_item: select_cart_items,
+  cart_total: select_cart_total
+});
+
+export default connect(mapStateToProps, null)(Checkout);
--- client/src/redux/selectors/cart.js
+++ client/src/redux/selectors/cart.js
@@ -16,3 +16,8 @@ export const select_cart_items_count = createSelector(
   [select_cart_items],
   cart_item => cart_item.reduce((acc, cart_item) => acc + cart_item.qty, 0)
 );
+
+export const select_cart_total = createSelector(
+  [select_cart_items],
+  cart_item => cart_item.reduce((acc, cart_item) => acc + cart_item.qty * cart_item.price, 0)
+);
