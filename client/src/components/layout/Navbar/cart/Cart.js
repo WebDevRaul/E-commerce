df39f5ea855c46295094fa197ec019121a5c5d32
TextError <savinraul1983@yahoo.com>
2019-08-13 17:03:43 +0100

    toggle icon on checkout page

--- client/src/components/layout/Navbar/cart/Cart.js
+++ client/src/components/layout/Navbar/cart/Cart.js
@@ -8,6 +8,7 @@ import CartItem from '../cart_item/cart_item';
 // Redux
 import { connect } from 'react-redux';
 import { select_cart_items } from '../../../../redux/selectors/cart';
+import { toggle_cart } from '../../../../redux/actions/cart';
 
 // Common
 import CustomButton from '../../../common/button/Custom_button';
@@ -15,7 +16,7 @@ import CustomButton from '../../../common/button/Custom_button';
 // Scss
 import './cart.scss';
 
-const Cart = ({ cart_item, history }) => (
+const Cart = ({ cart_item, history, toggle_cart }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {
@@ -27,18 +28,19 @@ const Cart = ({ cart_item, history }) => (
         <span className='empty-message'>Your cart is empty</span>
       }
     </div>
-    <div className='button-wrapper' onClick={() => history.push('/checkout')}>
+    <div className='button-wrapper' onClick={() => {history.push('/checkout'); toggle_cart()}}>
       <CustomButton value='GO TO CHECKOUT' isClass=''  />
     </div>
   </div>
 );
 
 Cart.propTypes = {
-  cart_item: PropTypes.array.isRequired
+  cart_item: PropTypes.array.isRequired,
+  toggle_cart: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
   cart_item: select_cart_items(state)
 });
 
-export default withRouter(connect(mapStateToProps, null)(Cart));
+export default withRouter(connect(mapStateToProps, { toggle_cart })(Cart));
