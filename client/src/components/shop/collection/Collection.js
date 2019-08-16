c78b1ea90315487cd2ddca117e36667a5e5ccdc9
TextError <savinraul1983@yahoo.com>
2019-08-16 14:45:02 +0100

    collection routes

--- client/src/components/shop/collection/Collection.js
+++ client/src/components/shop/collection/Collection.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // Components
 import CollectionItem from '../collection_item/Collection_Item';
 
+// Redux
+import { connect } from 'react-redux';
+import { select_collection } from '../../../redux/selectors/shop';
+
 // Scss
 import './collection.scss';
 
-const Collection = () => (
+const Collection = ({ collection }) => (
   <div className='collection'>
-    <h2>collection page</h2>
+    <h2>collection</h2>
   </div>
 );
 
-export default Collection;
+Collection.propTypes = {
+  collection: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: select_collection(ownProps.match.params.collection_id)(state)
+});
+
+export default connect( mapStateToProps, null )(Collection);
