57b2a7866ef3c6cc3d1abd1283b81ceebbe85bbb
TextError <savinraul1983@yahoo.com>
2019-08-21 15:58:43 +0100

    register hooks

--- client/src/components/auth/register/Register.js
+++ client/src/components/auth/register/Register.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 // Components
 import CustomButton from '../../common/button/Custom_button';
@@ -10,75 +10,71 @@ import { auth, createUserProfileDocument } from '../../../firebase/utils';
 // Scss
 import './register.scss';
 
-class Register extends Component {
-  state = {
+const Register = () => {
+  const [state, setState] = useState({ 
     name: '',
     email: '',
     password: '',
     password2: ''
+   });
+  const { name, email, password, password2 } = state;
+
+  const onChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value })
   }
 
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
-  };
-
-  onSubmit = async e => {
+  const onSubmit = async e => {
     e.preventDefault();
-    const { name, email, password, password2 } = this.state;
     // validate password
     if(password !== password2) return alert('Passwords don`t match');
     try{
       const { user } = await auth.createUserWithEmailAndPassword(email, password);
       await createUserProfileDocument(user, { name });
-      this.setState({ name: '', email: '', password: '', password2: '' });
+      setState({ name: '', email: '', password: '', password2: '' });
     }
     catch(error) { console.log(error) }
   }
-
-  render() {
-    const { name, email, password, password2 } = this.state;
-    return (
-      <div className='register'>
-        <h2 className='title'>I do not have a account</h2>
-        <span>Sign up with your email and password</span>
-        <form className='sign-up-form' onSubmit={this.onSubmit}>
-          <FormInput
-            type='text'
-            name='name'
-            value={name}
-            onChange={this.onChange}
-            label='name'
-          />
-          <FormInput
-            type='email'
-            name='email'
-            value={email}
-            onChange={this.onChange}
-            label='email'
-          />
-          <FormInput
-            type='password'
-            name='password'
-            value={password}
-            onChange={this.onChange}
-            label='password'
-          />
-          <FormInput
-            type='password'
-            name='password2'
-            value={password2}
-            onChange={this.onChange}
-            label='confirm password'
-          />
-          <CustomButton 
-            value='Sign Up'
-            type='submit'
-            isClass=''
-          />
-        </form>
-      </div>
-    )
-  }
+  return (
+    <div className='register'>
+      <h2 className='title'>I do not have a account</h2>
+      <span>Sign up with your email and password</span>
+      <form className='sign-up-form' onSubmit={onSubmit}>
+        <FormInput
+          type='text'
+          name='name'
+          value={name}
+          onChange={onChange}
+          label='name'
+        />
+        <FormInput
+          type='email'
+          name='email'
+          value={email}
+          onChange={onChange}
+          label='email'
+        />
+        <FormInput
+          type='password'
+          name='password'
+          value={password}
+          onChange={onChange}
+          label='password'
+        />
+        <FormInput
+          type='password'
+          name='password2'
+          value={password2}
+          onChange={onChange}
+          label='confirm password'
+        />
+        <CustomButton 
+          value='Sign Up'
+          type='submit'
+          isClass=''
+        />
+      </form>
+    </div>
+  )
 }
 
 export default Register;
