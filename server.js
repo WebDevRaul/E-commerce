d260fcaaa71da8be87bb2ad9aacb062de4c793a8
TextError <savinraul1983@yahoo.com>
2019-08-22 11:52:27 +0100

    payment Route

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 if(process.env.NODE_ENV !== 'production') require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -25,3 +26,14 @@ app.listen(PORT, error => {
   if(error) throw error;
   console.log(`Server running on port ${PORT}`)
 });
+
+
+app.post('/payment', (req, res) => {
+  const { token, amount } = req.body;
+  const body = { source: token.id, amount, currency: 'usd' };
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if(stripeErr) return res.status(500).send({error: stripeErr});
+    res.status(200).send({ success: stripeRes })
+  })
+})
